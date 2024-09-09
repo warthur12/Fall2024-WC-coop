607b44a86b916a7b06343eaec4f0266c9f6399f4
William Arthur <warthur@kentic-vision.com>
2024-09-09 15:58:23 -0400

    tested layout in graphql-demo

---
+++ projects/graphql-demo/client/src/app/pages/layout.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Button, ConfigProvider, Space } from 'antd';
+import "./globals.css";
+import Link from "next/link";
+
+import NavButton from "@/components/NavButton";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GraphQL-demo",
+  description: "WBA GraphQL-demo",
+};
+
+export default function RootLayout({
+  children,
+}: React.PropsWithChildren) {
+  return (
+    <html lang="en">
+      <AntdRegistry>
+        <ConfigProvider
+          theme={{
+            components: {
+              Button: {
+                defaultHoverBorderColor: "orange",
+                defaultHoverColor: "black"
+              }
+            }
+          }}
+        >
+          <body className={inter.className}>
+            <div>
+              <Link href={"/pages"}>
+                <h1 className="text-center font-bold text-6xl">
+                  Home
+                </h1>
+              </Link>
+            </div>
+            <div className="flex flex-row">
+              <NavButton page="pages/page1"></NavButton>
+              <NavButton page="pages/page2"></NavButton>
+              <NavButton page="pages/page3"></NavButton>
+              <NavButton page="pages/page4"></NavButton>
+            </div>
+            {children}
+          </body>
+        </ConfigProvider>
+      </AntdRegistry>
+    </html>
+  );
+}
